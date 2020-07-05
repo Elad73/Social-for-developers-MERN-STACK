66de996b23d3912fbdb1fdd0797c441bae07efcf
EladRon <elad.ron.g@gmail.com>
2020-07-05 14:01:49 +0300

    adding the profile refucer with get profile action

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
 
-const Dashboard = (props) => {
+// Since we want each time the Dashboard loads to get the profile, we are using the useEffect hook.
+// Since we want it to load only once, we are passing the empty array as the second parameter to the 'useEffect' function.
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+    useEffect(() => {
+        getCurrentProfile();
+    }, []);
     return <div>Dashboard</div>;
 };
 
-Dashboard.propTypes = {};
+Dashboard.propTypes = {
+    getCurrentProfile: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    profile: PropTypes.object.isRequired
+};
 
-export default Dashboard;
+const mapStateToProps = (state) => ({
+    auth: state.auth,
+    profile: state.profile
+});
+
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);

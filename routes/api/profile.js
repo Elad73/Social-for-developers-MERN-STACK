f62a37faa9d7901ae66c38df228fe6730e32a644
EladRon <elad.ron.g@gmail.com>
2020-07-09 10:19:52 +0300

    Delete education, experience and Account

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,7 @@ const request = require('request');
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 const keys = require('../../config/keys');
 const router = express.Router();
 
@@ -153,7 +154,9 @@ router.get('/user/:user_id', async (req, res) => {
 // @access Private
 router.delete('/', auth, async (req, res) => {
     try {
-        // @todo - remove user's posts
+        // Remove user's posts
+        await Post.deleteMany({ user: req.user.id });
+
         // Remove profile
         await Profile.findOneAndRemove({ user: req.user.id });
 
